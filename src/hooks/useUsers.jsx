9d551bf55d0a2ccb18af31391d670881e4edea9f
DmitryLavrov <d3236562@mail.ru>
2022-01-08 12:45:57 +0300

    Bugs fixed

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import userService from '../services/user.service'
 import { toast } from 'react-toastify'
+// import { useAuth } from './useAuth'
 
 const UserContext = React.createContext()
 
@@ -9,12 +10,24 @@ export const useUser = () => useContext(UserContext)
 
 const UserProvider = ({children}) => {
   const [users, setUsers] = useState([])
+  // const {currentUser} = useAuth()
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
 
   useEffect(() => {
     getUsers()
   }, [])
+
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     // =========================
+  //     console.log('users:', users)
+  //     console.log(':currentUser', currentUser)
+  //     // =========================
+  //     const newUsers = [...users].map(u => (u._id === currentUser._id ? currentUser : u))
+  //     setUsers(newUsers)
+  //   }
+  // }, [currentUser])
 
   useEffect(() => {
     if (error !== null) {
