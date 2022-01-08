bd64868202c7a977cd924551f06ec446ab5e83f6
DmitryLavrov <d3236562@mail.ru>
2022-01-08 17:24:51 +0300

    v6 - Pattern: get actual data for qualities - isOutdated()

--- src/components/ui/qualities/qualities.jsx
+++ src/components/ui/qualities/qualities.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { useSelector } from 'react-redux'
-import { getQualities, getQualitiesLoadingStatus } from '../../../store/qualities'
+import { useDispatch, useSelector } from 'react-redux'
+import { getQualities, getQualitiesLoadingStatus, loadQualitiesList } from '../../../store/qualities'
 
 const Qualities = ({qualities: qualitiesId}) => {
+  const dispatch = useDispatch()
   const qualities = useSelector(getQualities())
   const isLoading = useSelector(getQualitiesLoadingStatus())
+
+  useEffect(() => {
+    dispatch(loadQualitiesList())
+  })
 
   if (isLoading) {
     return 'Loading...'
--- src/store/qualities.js
+++ src/store/qualities.js
@@ -4,7 +4,8 @@ import qualityService from '../services/quality.service'
 const initialState = {
   entities: null,
   isLoading: true,
-  error: null
+  error: null,
+  lastFetch: null
 }
 
 const qualitiesSlice = createSlice({
@@ -17,6 +18,7 @@ const qualitiesSlice = createSlice({
     qualitiesReceived(state, action) {
       state.entities = action.payload
       state.isLoading = false
+      state.lastFetch = Date.now()
     },
     qualitiesRequestFailed(state, action) {
       state.error = action.payload
@@ -27,13 +29,22 @@ const qualitiesSlice = createSlice({
 
 const {qualitiesRequested, qualitiesReceived, qualitiesRequestFailed} = qualitiesSlice.actions
 
-export const loadQualitiesList = () => async (dispatch) => {
-  dispatch(qualitiesRequested())
-  try {
-    const {content} = await qualityService.get()
-    dispatch(qualitiesReceived(content))
-  } catch (err) {
-    dispatch(qualitiesRequestFailed(err.message))
+function isOutdated(date) {
+  if (Date.now() > (date + 10 * 60 * 1000)) {
+    return true
+  }
+  return false
+}
+
+export const loadQualitiesList = () => async (dispatch, state) => {
+  if (isOutdated(state().qualities.lastFetch)) {
+    dispatch(qualitiesRequested())
+    try {
+      const {content} = await qualityService.get()
+      dispatch(qualitiesReceived(content))
+    } catch (err) {
+      dispatch(qualitiesRequestFailed(err.message))
+    }
   }
 }
 
