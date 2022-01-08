7666f821eac7cfe5da4d04fc6699dc3f0331bb9d
DmitryLavrov <d3236562@mail.ru>
2022-01-08 14:31:22 +0300

    v4 - qualitiesSlice.actions implemented

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
@@ -13,8 +13,15 @@ import AuthProvider from './hooks/useAuth'
 import ProtectedRoute from './components/common/protectedRoute'
 import Logout from './layouts/logout'
 import EditUserPage from './components/page/editUserPage/editUserPage'
+import { useDispatch } from 'react-redux'
+import { loadQualitiesList } from './store/qualities'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(loadQualitiesList())
+  }, [])
+
   return (
     <div>
       <AuthProvider>
--- src/store/qualities.js
+++ src/store/qualities.js
@@ -1,14 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit'
+import qualityService from '../services/quality.service'
 
 const initialState = {
   entities: null,
-  isLoading: true
+  isLoading: true,
+  error: null
 }
 
 const qualitiesSlice = createSlice({
   name: 'qualities',
-  initialState
+  initialState,
+  reducers: {
+    qualitiesRequested(state) {
+      state.isLoading = true
+    },
+    qualitiesReceived(state, action) {
+      state.entities = action.payload
+      state.isLoading = false
+    },
+    qualitiesRequestFailed(state, action) {
+      state.error = action.payload
+      state.isLoading = false
+    }
+  }
 })
+
+const {qualitiesRequested, qualitiesReceived, qualitiesRequestFailed} = qualitiesSlice.actions
+
+export const loadQualitiesList = () => async (dispatch) => {
+  dispatch(qualitiesRequested())
+  try {
+    const {content} = await qualityService.get()
+    dispatch(qualitiesReceived(content))
+  } catch (err) {
+    dispatch(qualitiesRequestFailed(err.message))
+  }
+}
 
 const {reducer: qualitiesReducer} = qualitiesSlice
 
